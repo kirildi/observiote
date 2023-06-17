c932cb11b651c4b18e11d94aa053cec0b6aa4235
Kiril Ivanov <kiril.ivanov91@gmail.com>
2023-06-18 00:09:12 +0300

    feat: add device store
    To store a deviceList state, and to access it from other components
    
    Resolves #56

---
+++ src/interfaces/DeviceInterface.ts
@@ -0,0 +1,11 @@
+export interface DeviceInterface {
+  deviceId: string;
+  deviceName: string;
+  deviceImage: string;
+  deviceHash: string;
+  deviceDescription: string;
+  deviceCoordinates: string;
+  deviceCrateDate: string;
+  deviceLastModifyDate: string;
+  userName: string;
+}
