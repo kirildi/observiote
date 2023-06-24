6594eb16b12a51abc9834942f710a330f4d0d9d5
Kiril Ivanov <kiril.ivanov91@gmail.com>
2023-06-25 00:38:30 +0300

    refactor: thermometer component
    Duplication of for loop
    Change in the styling
    
    Closes #60

--- src/components/sensors/Thermometer.vue
+++ src/components/sensors/Thermometer.vue
@@ -1,93 +1,52 @@
 <script setup lang="ts">
-import { onMounted, ref, watchEffect } from "vue";
-import DefaultSensor from "./DefaultSensor.vue";
+  import { onMounted, ref, watchEffect } from "vue";
+  import DefaultSensor from "./DefaultSensor.vue";
+  import { SensorDataValue } from "../../types/SensorDataType";
 
-const props = defineProps<{
-  id: number;
-  type: string;
-  data: any[];
-}>();
-const isType = ref("basic");
-const dataNow = ref("");
-const degree = ref("\u{00B0}C");
+  const props = defineProps<{
+    id: number;
+    type: string;
+    data: any[];
+  }>();
+  const isType = ref("basic");
+  const dataNow = ref("");
+  const degree = ref("\u{00B0}C");
 
-onMounted(() => {
-  isType.value = props.type;
-  for (let i = 0; i < props.data.length; i += 1) {
-    if (props.data[i].sensorId.sensorId === props.id) {
-      dataNow.value = props.data[i].sensorData;
-      break;
-    }
+  function extractSensorData(dataObj: any[]): any {
+    const record = dataObj.filter((dataRecord) => {
+      dataRecord.sensorId.sensorId === dataRecord.id;
+    });
+
+    if (!record.length) return 0;
+
+    return record[0].sensorData;
   }
-});
-watchEffect(() => {
-  for (let i = 0; i < props.data.length; i += 1) {
-    if (props.data[i].sensorId.sensorId === props.id) {
-      dataNow.value = props.data[i].sensorData;
-      break;
-    }
-  }
-});
+
+  onMounted(() => {
+    isType.value = props.type;
+    dataNow.value = extractSensorData(props.data);
+  });
+  watchEffect(() => {
+    dataNow.value = extractSensorData(props.data);
+  });
 </script>
 <template>
   <div v-if="isType === 'basic'" class="therm-container">
     <default-sensor :id="'therm_' + id" :default-data="dataNow" />
   </div>
-  <div v-else class="therm-container">
-    <div class="therm-body"></div>
-    <div class="therm-body-circle"></div>
-    <div class="therm-cover"></div>
-    <div class="therm-cover-circle"></div>
-    <div>
-      <span class="therm-value" style="left: 60%; top: 10%">max: <span style="color: #ffb4b4">0</span>{{ degree }}</span>
-      <span class="therm-value" style="left: 15%; top: 20%">{{ dataNow }}{{ degree }}</span>
-      <span class="therm-value" style="left: 60%; bottom: 10%">min: <span style="color: #7ed9f3">0</span>{{ degree }}</span>
+  <div v-else class="therm__container h-32">
+    <div class="h-16 grid grid-rows-2 grid-flow-col justify-items-center gap-0">
+      <span class="therm__value row-span-2">{{ dataNow }}{{ degree }}</span>
+      <span class="therm__value col-span-1">max: <span class="text-red-300 text-center">0</span>{{ degree }}</span>
+      <span class="therm__value col-span-1">min: <span class="text-cyan-300 text-center">0</span>{{ degree }}</span>
+    </div>
+
+    <div class="h-16 relative">
+      <div class="therm__body w-full h-8 bg-gray-100 rounded-full"></div>
+      <div class="therm__body__circle w-14 h-14 absolute bg-gray-100 rounded-full left-0 -top-3"></div>
+      <div class="therm__cover w-8/12 h-6 bg-red-600 rounded-full absolute left-1 top-1"></div>
+      <div class="therm__cover__circle w-12 h-12 absolute bg-red-600 rounded-full left-1 -top-2"></div>
     </div>
   </div>
 </template>
-<style scoped>
-.therm-container {
-  width: 100%;
-  height: 100%;
-  padding: 1em;
-  display: flex;
-  justify-content: center;
-  flex-wrap: nowrap;
-  position: relative;
-}
-.therm-body {
-  height: 90%;
-  width: 5%;
-  background-color: #f4efde;
-  border-radius: 10em;
-}
-
-.therm-body-circle {
-  width: 11%;
-  height: 20%;
-  position: absolute;
-  background-color: #f4efde;
-  border-radius: 50%;
-  bottom: 16px;
-}
-.therm-cover {
-  height: 70%;
-  width: 3%;
-  background-color: #ff1c1c;
-  border-radius: 10em;
-  position: absolute;
-  margin-top: 1%;
-}
-
-.therm-cover-circle {
-  width: 10%;
-  height: 18%;
-  position: absolute;
-  background-color: #ff1c1c;
-  border-radius: 50%;
-  bottom: 18px;
-}
-.therm-value {
-  position: absolute;
-}
-</style>
+<style scoped></style>
