85b76f75f183d5a3e15d2833a73d5e9ad9ab1cbf
Kiril Ivanov <kiril.ivanov91@gmail.com>
2023-07-17 23:59:35 +0300

    refactor: DefaultSensor component
    Remove old CSS classes
    Using the proper data variable
    "default-data" property is  just "data" now
    New hight for the "sensor__body" container
    
    Closes #79

--- src/components/sensors/DefaultSensor.vue
+++ src/components/sensors/DefaultSensor.vue
@@ -1,52 +1,45 @@
 <script setup lang="ts">
-import { onMounted, ref, watchEffect } from "vue";
+  import { Ref, onMounted, ref, watchEffect } from "vue";
+  import { SensorDataValue } from "../../types/SensorDataType";
 
-const props = defineProps<{
-  id: string;
-  defaultData: string;
-}>();
+  const props = defineProps<{
+    id: number | string;
+    data: SensorDataValue[];
+  }>();
 
-const degree = ref("\u{00B0}C");
-const isDataType = ref(false);
+  const dataNow: Ref<SensorDataValue[]> = ref([]);
+  const isDataType = ref(false);
 
-onMounted(() => {});
-watchEffect(() => {});
+  function verifySensorData(dataObj: SensorDataValue[]): SensorDataValue[] {
+    if (dataObj === undefined) return [{ max: 0, min: 0, now: 0 }];
+    return dataObj;
+  }
+
+  onMounted(() => {});
+  watchEffect(() => {
+    dataNow.value = verifySensorData(props.data);
+  });
 </script>
 
 <template>
-  <div v-if="isDataType" class="therm-basic">
-    <div class="therm-value">
-      <span class="w3-col s9 l9">{{ defaultData }}</span>
-      <!--<span class="w3-col s3 l3" style="margin-left: 0,5em"> {{ degree }}</span>-->
+  <div v-if="!isDataType" class="default__container relative w-full h-full flex-row justify-center items-center">
+    <div class="therm__value absolute top-4 w-56 h-auto flex justify-center text-2xl">
+      <span class="">{{ dataNow[0]?.now ?? 0 }}</span>
     </div>
-    <canvas :id="id" class="therm-chart"></canvas>
   </div>
-  <div v-else class="therm-basic">
-    <div class="therm-value">
-      <span class="w3-col s9 l9">{{ defaultData }}</span>
+  <div v-else class="therm__basic h-20">
+    <div class="therm__value flex items-center justify-center">
+      <span class=" ">{{ dataNow[0]?.now ?? 0 }}</span>
+      <!-- <canvas :id="id" class="therm-chart"></canvas> -->
       <!--<span class="w3-col s3 l3" style="margin-left: 0,5em"> {{ degree }}</span>-->
     </div>
   </div>
 </template>
 
 <style scoped>
-.therm-basic {
-  position: relative;
-  width: 100%;
-  height: 100%;
-}
-.therm-chart {
-  width: 100% !important;
-  height: 100% !important;
-  opacity: 0.6;
-}
-
-.therm-value {
-  position: absolute;
-  left: 50%;
-  top: 46%;
-  transform: translate(-50%, -54%);
-  font-size: 2em;
-  z-index: 1000;
-}
+  .therm-chart {
+    width: 100% !important;
+    height: 100% !important;
+    opacity: 0.6;
+  }
 </style>
