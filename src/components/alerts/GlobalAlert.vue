fbe1167ce997aa4ebe9372f9791f008d49ba08f4
Kiril Ivanov <kiril.ivanov91@gmail.com>
2023-08-06 00:06:43 +0300

    refactor: GlobalAlert component
    - Comparing against Enum value instead of plain text
    - alertHidden value is renamed for better readability

--- src/components/alerts/GlobalAlert.vue
+++ src/components/alerts/GlobalAlert.vue
@@ -1,24 +1,21 @@
 <script setup lang="ts">
   import { reactive, ref, watchEffect } from "vue";
   import { useAlertsStore } from "../../stores/globalAlertStore";
+  import { GlobalAlertTypeEnum } from "../../enums/GlobalAlertTypeEnum";
 
   const store = useAlertsStore();
-  const alertHidden = ref(true);
+  const isAlertVisible = ref(true);
 
   const alertProps = reactive({
-    alertHeader: "ERROR",
+    alertHeader: `${GlobalAlertTypeEnum.error}`,
     alertColor: "alert-error",
     alertCode: "0000",
     alertMessage: "",
   });
 
   function handleAlert() {
-    if (store.alertType === "ERROR") {
-      alertProps.alertHeader = "ERROR: ";
-      alertProps.alertColor = "alert-error";
-    }
-    if (store.alertType === "WARNING") {
-      alertProps.alertHeader = "WARNING: ";
+    if (store.alertType === GlobalAlertTypeEnum.warning) {
+      alertProps.alertHeader = `${GlobalAlertTypeEnum.error}: `;
       alertProps.alertColor = "alert-warning";
     }
     alertProps.alertCode = store.alertCode.toString();
@@ -27,14 +24,14 @@
   watchEffect(() => {
     if (store.triggered) {
       handleAlert();
-      alertHidden.value = true;
+      isAlertVisible.value = true;
     } else {
-      alertHidden.value = false;
+      isAlertVisible.value = false;
     }
   });
 </script>
 <template>
-  <div v-show="alertHidden" class="container relative p-4">
+  <div v-show="isAlertVisible" class="container relative p-4">
     <div :class="alertProps.alertColor">
       <span>{{ alertProps.alertHeader }}[{{ alertProps.alertCode }}]: </span>
       <span>{{ alertProps.alertMessage }}</span>
